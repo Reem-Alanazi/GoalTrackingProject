8293f74d4318da29fd1c0a270c5d0c2de7d48b53
Reem Alanazi <92260250+Reem-Alanazi@users.noreply.github.com>
2022-01-07 19:29:53 +0300

    create RegisterActivity

---
+++ app/src/main/java/com/reem/goaltrackingproject/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.reem.goaltrackingproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}

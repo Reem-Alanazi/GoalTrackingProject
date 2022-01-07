b7747f3e5da598c93bef4b74c7408db49c673c22
Reem Alanazi <92260250+Reem-Alanazi@users.noreply.github.com>
2022-01-07 20:52:47 +0300

    showErrorSnackBar

--- app/src/main/java/com/reem/goaltrackingproject/BaseActivity.kt
+++ app/src/main/java/com/reem/goaltrackingproject/BaseActivity.kt
@@ -38,5 +38,18 @@ class BaseActivity : AppCompatActivity() {
     }
 
 
+    fun showErrorSnackBar(message: String) {
+        val snackBar =
+            Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
+        val snackBarView = snackBar.view
+        snackBarView.setBackgroundColor(
+            ContextCompat.getColor(
+                this@BaseActivity,
+                R.color.snackbar_error_color
+            )
+        )
+        snackBar.show()
+    }
+
 }
 

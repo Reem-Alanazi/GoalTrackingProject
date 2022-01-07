12a8fcc20303bbcd582674f5735931c9775150ab
Reem Alanazi <92260250+Reem-Alanazi@users.noreply.github.com>
2022-01-07 20:20:36 +0300

    set up the action bar

--- app/src/main/java/com/reem/goaltrackingproject/RegisterActivity.kt
+++ app/src/main/java/com/reem/goaltrackingproject/RegisterActivity.kt
@@ -1,12 +1,19 @@
 package com.reem.goaltrackingproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+
+        btn_sign_up_intro.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
 
     }
 }

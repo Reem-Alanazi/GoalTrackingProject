4c53985441ceb30c42f02e7486f00154d539276b
Reem Alanazi <92260250+Reem-Alanazi@users.noreply.github.com>
2022-01-07 21:36:29 +0300

    make userRegisteredSuccess function

--- app/src/main/java/com/reem/goaltrackingproject/SignUpActivity.kt
+++ app/src/main/java/com/reem/goaltrackingproject/SignUpActivity.kt
@@ -3,9 +3,11 @@ package com.reem.goaltrackingproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
@@ -31,5 +33,21 @@ class SignUpActivity : AppCompatActivity() {
         }
 
         toolbar_sign_up_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+
+    fun userRegisteredSuccess(){
+        Toast.makeText(
+            this, "You have successfully registered.",
+            Toast.LENGTH_SHORT
+        ).show()
+        hideProgressDialog()
+        /*
+         Here the new user registered is automatically signed-in so I just signOut the user from firebase
+         and send him to registered activity for SignIn */
+        FirebaseAuth.getInstance().signOut()
+        finish()
+
+
     }
 }

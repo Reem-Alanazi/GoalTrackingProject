380816ca3ab6453dd07e399437f64c636137d9d2
Reem Alanazi <92260250+Reem-Alanazi@users.noreply.github.com>
2022-01-07 21:17:09 +0300

    make signInRegisteredUser function

--- app/src/main/java/com/reem/goaltrackingproject/SignInActivity.kt
+++ app/src/main/java/com/reem/goaltrackingproject/SignInActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.view.WindowManager
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_in.*
 
@@ -17,6 +19,7 @@ class SignInActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
+        auth = FirebaseAuth.getInstance()
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -24,9 +27,40 @@ class SignInActivity : BaseActivity() {
         )
 
         setupActionBar()
+
+        btn_sign_in.setOnClickListener {
+            signInRegisteredUser()
+        }
     }
 
 
+
+    private fun signInRegisteredUser(){
+
+        val email : String = et_email_signIn.text.toString().trim {it <= ' '}
+        val password : String = et_password_signIn.text.toString().trim { it <= ' '}
+
+        if (validateForm(email, password)){
+            showProgressDialog(resources.getString(R.string.please_wait))
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this){
+                        task -> hideProgressDialog()
+                    if (task.isSuccessful){
+                        Log.d("SignIn","SignInWithEmailAndPassword")
+                        val user = auth.currentUser
+                        // go main activity
+                        startActivity(Intent(this,MainActivity::class.java))
+                    } else{
+                        Log.w("SignIn","signInWithEmailAndPassword",task.exception)
+                        Toast.makeText(baseContext,"Authentication failed.", Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+
+        }
+
+    }
 
 
     private fun setupActionBar() {

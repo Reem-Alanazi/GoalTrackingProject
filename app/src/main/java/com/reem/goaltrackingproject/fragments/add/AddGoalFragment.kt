e0fd109eb84e05d83026f2a2554a7f6826c71341
Reem Alanazi <92260250+Reem-Alanazi@users.noreply.github.com>
2022-01-09 10:51:22 +0300

    search view

--- app/src/main/java/com/reem/goaltrackingproject/fragments/add/AddGoalFragment.kt
+++ app/src/main/java/com/reem/goaltrackingproject/fragments/add/AddGoalFragment.kt
@@ -16,6 +16,8 @@ class AddGoalFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        //
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_goal, container, false)
     }

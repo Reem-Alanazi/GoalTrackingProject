5e20449f9428681aff38b7b11b6318a033a572ea
Reem Alanazi <92260250+Reem-Alanazi@users.noreply.github.com>
2022-01-09 11:06:54 +0300

    set up menu to Add Goal fragment

--- app/src/main/java/com/reem/goaltrackingproject/fragments/update/UpdateGoalFragment.kt
+++ app/src/main/java/com/reem/goaltrackingproject/fragments/update/UpdateGoalFragment.kt
@@ -1,10 +1,8 @@
 package com.reem.goaltrackingproject.fragments.update
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.reem.goaltrackingproject.R
 
 class UpdateGoalFragment : Fragment() {
@@ -15,7 +13,15 @@ class UpdateGoalFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+        // set up
+        setHasOptionsMenu(true)
+
         return inflater.inflate(R.layout.fragment_update_goal, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_goal_fragment_menu, menu)
     }
 
 }

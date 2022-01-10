0484df103590e87bcbfeb5d5c4cf3f046f0b1c6d
Reem Alanazi <92260250+Reem-Alanazi@users.noreply.github.com>
2022-01-10 03:46:22 +0300

    create repository class

---
+++ app/src/main/java/com/reem/goaltrackingproject/data/GoalRepository.kt
@@ -0,0 +1,12 @@
+package com.reem.goaltrackingproject.data
+
+import androidx.lifecycle.LiveData
+
+class GoalRepository (private val goalDao: GoalDao){
+
+    val getAllData:LiveData<List<GoalData>> = goalDao.getAllDate()
+
+    suspend fun insertData(goalData : GoalData){
+        goalDao.insertDate(goalData)
+    }
+}

6020be8743d4207c67fb879b0539696d727139f5
Reem Alanazi <92260250+Reem-Alanazi@users.noreply.github.com>
2022-01-10 04:01:36 +0300

    create viewmodel

---
+++ app/src/main/java/com/reem/goaltrackingproject/data/GoalViewModel.kt
@@ -0,0 +1,31 @@
+package com.reem.goaltrackingproject.data
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+// extend android view model
+
+class GoalViewModel(application: Application):AndroidViewModel(application) {
+
+    private val goalDao = GoalLocalDatabase.getDatabase(application).goalDao()
+    private val repository : GoalRepository
+
+       // this val will holds live data object
+    private  val getAllData:LiveData<List<GoalData>>
+
+    init {
+        repository = GoalRepository(goalDao)
+        getAllData=repository.getAllData
+    }
+
+    // run this fun in background thread
+    fun insertData(goalData: GoalData){
+        viewModelScope.launch(Dispatchers.IO){
+            repository.insertData(goalData)
+        }
+    }
+}
